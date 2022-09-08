ea2dcb6f4c3c74157a1e40d44e474c26f93087d9
MariaRange <ticqraeg001125mdgnsda1@gmail.com>
2022-09-08 12:19:14 -0500

    12:19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReferenciasComponent } from './referencias/referencias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReferenciasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/referencias/referencias.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-referencias',
+  templateUrl: './referencias.component.html',
+  styleUrls: ['./referencias.component.css']
+})
+export class ReferenciasComponent{
+
+
+}
